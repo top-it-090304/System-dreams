50d26d5370692fbbbeb15174ce0d8dcac124c202
PolinaSedikova <sfp4y2sxv8@privaterelay.appleid.com>
2026-02-24 18:12:56 +0600

    починено разрешение экрана
    перерисовано своими руками меню, добавлены сцены настроек и туториала с возможностью выхода, но без рисуноков, отлажены размеры персонажей

---
+++ system-dreams/ui/tutorial/go_back_button.gd
@@ -0,0 +1,13 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _process(delta: float) -> void:
+	pass
+	
+	
+func _on_pressed() -> void:
+	get_tree().change_scene_to_file("res://ui/main_menu/main_menu.tscn")
