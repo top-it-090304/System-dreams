f3970f46e018c94d6094f8fae9bc7baa234788b1
dekrill <zereksplay56740@gmail.com>
2026-03-11 20:36:35 +0600

    изменение опыта

--- system-dreams/src/Player/exp/exp.gd
+++ system-dreams/src/Player/exp/exp.gd
@@ -1,15 +1,12 @@
 extends Area2D
 
-@export var xp_amount: int = 1
+@export var xp_amount: int = 10
 
 func _ready() -> void:
-	# Подписываемся на сигнал, когда кто-то входит в зону опыта
 	body_entered.connect(_on_body_entered)
 
 
 func _on_body_entered(body: Node) -> void:
-	# Опыт подбирает только игрок
 	if body is Player:
 		body.add_xp(xp_amount)
 		queue_free()
-
