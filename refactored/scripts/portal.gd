c3db2a9f91893d16b9811c13e439c5cdda4a1887
dekrill <zereksplay56740@gmail.com>
2026-05-13 17:01:07 +0600

    добавлен экран при победе

--- refactored/scripts/portal.gd
+++ refactored/scripts/portal.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+const WIN_SCREEN_SCENE := preload("res://scenes/ui/win.tscn")
+
 var _player: Node2D = null
 
 @onready var _sprite: AnimatedSprite2D = $AnimatedSprite2D
@@ -7,12 +9,21 @@ var _player: Node2D = null
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
-
 	if _sprite:
 		_sprite.play(anim_normal)
-	
-	
+
 func _on_body_entered(body: Node) -> void:
 	if body is Player:
+		var final_time = body._run_time
+		var final_level = body.level
+		
+		get_tree().paused = true
+		
+		var win_screen = WIN_SCREEN_SCENE.instantiate()
+		
+		get_tree().root.add_child(win_screen)
+		
+		if win_screen.has_method("init_stats"):
+			win_screen.init_stats(final_time, final_level)
+		
 		queue_free()
-		get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
